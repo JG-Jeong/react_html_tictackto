3b30c97c956ee4652f596defd158852ff986ce89
JG-Jeong <jo128g@naver.com>
2023-10-09 23:34:08 +0900

    과거의 이동 표시하기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React, { startTransition } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from "react";
+import { ReactDOM } from "react";
 
-//자습서에 복붙하지말고 하나씩 치라고 해서 하나씩 침
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -12,12 +10,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  // 1. renderSquare 라는 함수가 아래에서 JSX로 불림.
-  // 2. 내장된 DOM <button> 컴포넌트에 있는  onClick prop은 클릭 이벤트 리스너 설정하라고 함
-  // 3. 버튼을 클릭하면 React는 Square의 render() 함수에 정의된 onClick 이벤트 핸들러를 호출함.
-  // 4. 이벤트 핸들러는 this.props.onClick() 를 호출합니다. Square의 onClick prop은 Board에서 정의됨.
-  // 5. Board에서 Square로 onClick={() => this.handleClick(i)} 를 전달했기 때문에 Square를 누르면 Board의 handleClick(i)를 호출
-
   renderSquare(i) {
     return (
       <Square
@@ -50,6 +42,80 @@ class Board extends React.Component {
   }
 }
 
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -69,67 +135,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history;
-    const current = this[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([{ squares: squares }]),
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current.squares);
-
-    let status;
-    if (winner) {
-      status = "Winner:" + winner;
-    } else {
-      status = "Next player:" + (this.state.xIsNext ? "X" : "O");
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>
-            {/*
-        Todo*/}
-          </ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ======================================== 이 아래 부분이 무슨뜻임?
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
